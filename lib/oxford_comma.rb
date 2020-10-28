96f34f8081e17182eacb381705de6befae8cc81d
David <dharri81@gmail.com>
2020-10-28 10:22:32 -0600

    commit tree 7e082353b675ec86127e4751d4af6c682831c81c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    array.join
 end
+
+def oxford_comma(array)
+    array.join(" and ")
+end
+
+def oxford_comma(array)
+    if array.size >= 3 
+      last = array.pop
+      array.push('and '+last)
+      array.join(', ')
+    elsif array.size >= 1
+      array.join(' and ')  
+    else
+      array.join  
+    end  
+  end
